3753bf3cadb98216ded7b0ad358626e04eaa38bd
Muhammed Samy <eng_muhamed_samy@yahoo.com>
2022-06-06 21:02:38 +0200

    work with pug template engine

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const admin = require('./routes/admin')
+const shop = require('./routes/shop')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
+//set global configuration
+app.set('view engine', "pug")
+app.set('views', "views")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// add static folder to serve some file without using express
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/admin", admin.router)
+app.use(shop)
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"))
+})
+
+app.listen(3000, () => console.log(`server running on port ${3000}`))
---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+// import * as express from 'express'
+
+const router = express.Router();
+
+const products = []
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "views", "add-product.html"))
+})
+
+router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title })
+    res.redirect('/')
+})
+
+exports.router = router;
+exports.products = products;
